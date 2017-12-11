e3df7edaaecf03d4d98357319eecb1328278f114
Rafael Ginard Aguilera <rginard@gamma05.aulas.gsyc.urjc.es>
2017-12-11 10:51:28 +0100

    Fichero parseado

---
+++ uaclient.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+class XMLHandler(ContentHandler):
+
+    def __init__(self):
+        self.config = {}
+        self.attrDict = {"account": ["username", "passwd"],
+                         "uaserver": ["ip","port"],
+                         "rtpaudio": ["port_r"],
+                         "regproxy": ["ip","port"],
+                         "log": ["path"],
+                         "audio": ["path"]}
+
+    def startElement(self, name, attrs):
+
+        if name in self.attrDict:
+            for atribute in self.attrDict[name]:
+                self.config[name + "_" + atribute] = attrs.get(atribute, "")
+
+    def Get_Tags(self):
+        return self.config
+
+if __name__ == "__main__":
+
+  parser = make_parser()
+  cHandler = XMLHandler()
+  parser.setContentHandler(cHandler)
+  parser.parse(open("ua1.xml"))
+  print(cHandler.Get_Tags())
