41edba981c06779f952b8b0bae39f0a6edcbce02
rafaginard <rafaginard@gmail.com>
2017-12-16 12:33:15 +0100

    Server basico

---
+++ uaserver.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Clase (y programa principal) para un servidor de eco en UDP simple
+"""
+
+import socketserver
+
+class EchoHandler(socketserver.DatagramRequestHandler):
+    """
+    Echo server class
+    """
+
+    def handle(self):
+        """
+        handle method of the server class
+        (all requests will be handled by this method)
+        """
+        self.wfile.write(b"Hemos recibido tu peticion")
+        for line in self.rfile:
+            print("El cliente nos manda ", line.decode('utf-8'))
+
+if __name__ == "__main__":
+    # Listens at localhost ('') port 6001
+    # and calls the EchoHandler class to manage the request
+    serv = socketserver.UDPServer(('', 5555), EchoHandler)
+
+    print("Lanzando servidor UDP de eco...")
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("Finalizado servidor")
