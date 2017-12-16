a1d5f8ce25082c89dcc5512ae792e4db55fdcc89
rafaginard <rafaginard@gmail.com>
2017-12-16 12:33:36 +0100

    Cliente envia Register

--- uaclient.py
+++ uaclient.py
@@ -29,11 +29,27 @@ class XMLHandler(ContentHandler):
 
 
 
-
 if __name__ == "__main__":
 
   parser = make_parser()
   cHandler = XMLHandler()
   parser.setContentHandler(cHandler)
-  parser.parse(open("ua1.xml"))
-  print(cHandler.Get_Tags())
+  parser.parse(open(sys.argv[1]))
+
+  SERVER = cHandler.config["uaserver_ip"]
+  PORT = int(cHandler.config["uaserver_puerto"])
+  LINE = "HOLA QUE TAL"
+  EXPIRES = sys.argv[3]
+
+# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+    my_socket.connect((SERVER, PORT))
+    print("Enviando:", LINE)
+    if sys.argv[2] == "REGISTER":
+        DATA = ("REGISTER sip:" + LINE + " SIP/2.0\r\nExpires: " +
+                EXPIRES + "\r\n\r\n")
+        my_socket.send(bytes(DATA, "utf-8"))
+    data = my_socket.recv(1024)
+    print('Recibido -- ', data.decode('utf-8'))
+
+print("Socket terminado.")
