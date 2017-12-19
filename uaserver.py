7b7166dd91d67f36e5928fcd1287fc370fa7ab32
rafaginard <rafaginard@gmail.com>
2017-12-19 17:55:09 +0100

    Server con diccionario, arreglar register

--- uaserver.py
+++ uaserver.py
@@ -4,15 +4,40 @@
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 import os
 import socketserver
 import sys
 
+class XMLHandler(ContentHandler):
+
+    def __init__(self):
+        self.config = {}
+        self.attrDict = {"account": ["username", "passwd"],
+                         "uaserver": ["ip","puerto"],
+                         "rtpaudio": ["puerto"],
+                         "regproxy": ["ip","puerto"],
+                         "log": ["path"],
+                         "audio": ["path"]}
+
+    def startElement(self, name, attrs):
+
+        if name in self.attrDict:
+            for atribute in self.attrDict[name]:
+                self.config[name + "_" + atribute] = attrs.get(atribute, "")
+
+    def Get_Tags(self):
+
+        return self.config
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
+
+    dicc_Data = {}
+
     def Comprobar_Peticion(self):
         if len(self.DATA) >= 3:
             condition_sip = self.DATA[1].split(":")[0] == ("sip")
@@ -24,6 +49,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 self.check = True
         return self.check
 
+
     def handle(self):
         nonce = b"898989898798989898989"
         self.check = False
@@ -31,13 +57,14 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         print(line.decode('utf-8'))
         if line:
             self.DATA = line.decode('utf-8').split(" ")
-    
+
             if self.Comprobar_Peticion():
                 if self.DATA[0] == "REGISTER":
                     self.wfile.write(b"SIP/2.0 401 Unauthorized\r\n")
                     self.wfile.write(b"WWW Authenticate:" +
                                      b"Digest nonce=" + nonce)
                     self.wfile.write(b"\r\n\r\n")
+                    #AÑADIR AL DICCIONARIO
                 elif self.DATA[0] == "INVITE":
                     self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
                     self.wfile.write(b"SIP/2.0 180 Ringing\r\n\r\n")
@@ -54,8 +81,17 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             pass
 
 if __name__ == "__main__":
+
+    parser = make_parser()
+    cHandler = XMLHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open(sys.argv[1]))
+
+    SERVER = cHandler.config["uaserver_ip"]
+    PORT = int(cHandler.config["uaserver_puerto"])
+
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(("", 5555), EchoHandler)
+    serv = socketserver.UDPServer((SERVER, PORT), EchoHandler)
     try:
         print("Listening...")
         serv.serve_forever()
