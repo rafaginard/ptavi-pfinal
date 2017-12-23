53ff9666711f81431791338a79dd70d38d32d85e
rafaginard <rafaginard@gmail.com>
2017-12-23 19:36:36 +0100

    funcion log implementada

--- uaclient.py
+++ uaclient.py
@@ -5,6 +5,7 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import socket
 import sys
+import time
 
 class XMLHandler(ContentHandler):
 
@@ -27,17 +28,29 @@ class XMLHandler(ContentHandler):
 
         return self.config
 
+def log(action, data):
+    Time_Format = time.strftime("%Y%m%d%H%M%S ",
+    time.gmtime(time.time()))
+    log_list = []
+    new_message = data.replace("\r\n", " ")
+    log_list.append(Time_Format + action +
+                    new_message + "\r\n")
+    print(log_list)
+
+
 # Metodo REGISTER
 def register():
-    DATA = ("REGISTER sip:" + User_Name + ":5555" + " SIP/2.0\r\n\r\n")
+    DATA = ("REGISTER sip:" + User_Name + ":5555" + " SIP/2.0\r\n" +
+            "Expires: " + EXPIRES + "\r\n\r\n")
     my_socket.send(bytes(DATA, "utf-8"))
-# ARREGLAR EL EXPIERES
+    log("Sent to ", DATA)
 
 def register_with_nonce():
     nonce = "123123212312321212123"
     DATA = ("REGISTER sip:" + User_Name + ":5555" + " SIP/2.0\r\n" +
             "Authorization: Digest response=" + nonce + "\r\n\r\n")
     my_socket.send(bytes(DATA, "utf-8"))
+    log("Sent to ", DATA)
 
 # Metodo INVITE
 def invite():
@@ -46,6 +59,12 @@ def invite():
             "v=0\r\no=" + User_Name + "\r\ns=misesion" +
             "\r\nt=0\r\nm=audio " + Audio_Puerto + " RTP")
     my_socket.send(bytes(DATA, "utf-8"))
+    log("Sent to ", DATA)
+
+def bye():
+    DATA = ("BYE sip:" + User_Name + ":5555" + " SIP/2.0\r\n\r\n")
+    my_socket.send(bytes(DATA, "utf-8"))
+    log("Sent to ", DATA)
 
 if __name__ == "__main__":
 
@@ -71,21 +90,29 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     elif sys.argv[2] == "INVITE":
         INVITATION = sys.argv[3]
         invite()
+    elif sys.argv[2] == "BYE":
+        USER = sys.argv[3]
+        bye()
     data = my_socket.recv(1024)
 
 # Recibe datos del servidor.
     Recieve = data.decode('utf-8').split(" ")
     if Recieve[1] == "200":
         print(data.decode('utf-8'))
+        log("Recieved from ", data.decode('utf-8'))
     elif Recieve[1] == "100":
         print(data.decode('utf-8'))
+        log("Recieved from ", data.decode('utf-8'))
         my_socket.connect((SERVER, PORT))
-        my_socket.send(bytes("REGISTER sip:" + User_Name + "@" + SERVER +
-                             " SIP/2.0", 'utf-8') + b'\r\n\r\n')
+        DATA = ("REGISTER sip:" + User_Name + "@" + SERVER +
+                " SIP/2.0" + "\r\n\r\n")
+        my_socket.send(bytes(DATA, "utf-8"))
         data = my_socket.recv(1024)
+        log("Sent to ", DATA)
     elif Recieve[1] == "401":
         my_socket.connect((SERVER, PORT))
         register_with_nonce()
         data = my_socket.recv(1024)
+        log("Recieved from ", data.decode('utf-8'))
     else:
         print(data.decode('utf-8'))
