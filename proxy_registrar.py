53327409747b1a9f49b93a5bf105e4c546630db8
rafaginard <rafaginard@gmail.com>
2017-12-25 19:43:51 +0100

    register, diccionario de usuarios implementado

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -43,7 +43,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                                     time.gmtime(time.time()))
         if self.dicc_Data:
             for user in self.dicc_Data:
-                tiempo = self.dicc_Data[user][1]
+                tiempo = self.dicc_Data[user][3]
                 dicc_Temp[user] = tiempo[9:]
             for user in dicc_Temp:
                 if dicc_Temp[user] < Time_Format:
@@ -54,6 +54,8 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         handle method of the server class
         (all requests will be handled by this method)
         """
+        Time_Format = time.strftime("%Y-%m-%d %H:%M:%S",
+                                    time.gmtime(time.time()))
         nonce = b"898989898798989898989"
         DATA = []
         self.check_server()
@@ -61,31 +63,45 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             DATA.append(line.decode('utf-8'))
         DATA = "".join(DATA).split()
         user = DATA[1].split(":")
+
         if DATA[0] == "REGISTER":
-            if len(DATA) == 5:
-                if int(DATA[4]) == 0:
-                    print("ENTRA AQUI")
-                    try:
-                        print("Usuario borrado:", user[1], "\r\n\r\n")
-                        del self.dicc_Data[user[1]]
-                        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-                    except KeyError:
-                        self.wfile.write(b"SIP/2.0 404 User Not Found\r\n\r\n")
-                elif int(DATA[4]) >= 0:
-                    print(" ".join(DATA), "\r\n\r\n")
-                    self.wfile.write(b"SIP/2.0 401 Unauthorized\r\n")
-                    self.wfile.write(b"WWW Authenticate:" +
-                                     b"Digest nonce=" + nonce)
-                    self.wfile.write(b"\r\n\r\n")
-            elif len(DATA) == 8:
+            if user[1] in self.dicc_Data:
                 tiempo_exp = float(DATA[4]) + time.time()
                 tiempo_exp = time.strftime("%Y-%m-%d %H:%M:%S",
                                            time.gmtime(tiempo_exp))
                 print(" ".join(DATA), "\r\n\r\n")
                 self.dicc_Data[user[1]] = (self.client_address[0],
+                                           self.client_address[1],
+                                           "Time Register: " + Time_Format,
                                            "Expires: " + tiempo_exp)
                 self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            else:
+                if len(DATA) == 5:
+                    if int(DATA[4]) == 0:
+                        try:
+                            print("Usuario borrado:", user[1], "\r\n\r\n")
+                            del self.dicc_Data[user[1]]
+                            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                        except KeyError:
+                            self.wfile.write(b"SIP/2.0 404 User Not Found\r\n\r\n")
+                    elif int(DATA[4]) >= 0:
+                        print(" ".join(DATA), "\r\n\r\n")
+                        self.wfile.write(b"SIP/2.0 401 Unauthorized\r\n")
+                        self.wfile.write(b"WWW Authenticate:" +
+                                         b"Digest nonce=" + nonce)
+                        self.wfile.write(b"\r\n\r\n")
+                elif len(DATA) == 8:
+                    tiempo_exp = float(DATA[4]) + time.time()
+                    tiempo_exp = time.strftime("%Y-%m-%d %H:%M:%S",
+                                               time.gmtime(tiempo_exp))
+                    print(" ".join(DATA), "\r\n\r\n")
+                    self.dicc_Data[user[1]] = (self.client_address[0],
+                                               self.client_address[1],
+                                               "Time Register: " + Time_Format,
+                                               "Expires: " + tiempo_exp)
+                    self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
 
+            print(self.dicc_Data)
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001
     # and calls the EchoHandler class to manage the request
@@ -100,8 +116,7 @@ if __name__ == "__main__":
         SERVER = cHandler.config["server_ip"]
     PORT = int(cHandler.config["server_puerto"])
     serv = socketserver.UDPServer((SERVER, PORT), SIPRegisterHandler)
-    try:
-        print("Listening...")
-        serv.serve_forever()
-    except:
-        sys.exit("Usage: python3 server.py IP port audio_file")
+
+    print("Server " + cHandler.config["server_name"] +
+          " listening at port " + str(PORT) + "...")
+    serv.serve_forever()
