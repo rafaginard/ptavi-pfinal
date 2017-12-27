b5568800d9edef2b99f12eddc4f1f0f7b8a1ffd9
rafaginard <rafaginard@gmail.com>
2017-12-27 19:33:45 +0100

    proxy recibe invites y envia al uaserver

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -8,6 +8,7 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import json
 import socketserver
+import socket
 import sys
 import time
 
@@ -49,6 +50,47 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                 if dicc_Temp[user] < Time_Format:
                     del self.dicc_Data[user]
 
+    def invite(self, DATA):
+
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+            user_to_send = DATA[1].split(":")[1]
+            user_who_invites = DATA[6].split("=")[1]
+            audio_port = DATA[10]
+            try:
+                user_to_send_ip = self.dicc_Data.get(user_to_send)[0]
+                user_to_send_port = self.dicc_Data.get(user_to_send)[1]
+                my_socket.connect((user_to_send_ip, int(user_to_send_port)))
+                Invitation = ("INVITE sip:" + user_to_send + " SIP/2.0\r\n" +
+                              "Content-Type: application/sdp\r\n\r\n" +
+                              "v=0\r\no=" + user_who_invites + "\r\ns=misesion" +
+                              "\r\nt=0\r\nm=audio " + audio_port + " RTP")
+                my_socket.send(bytes(Invitation, "utf-8"))
+                print(Invitation)
+
+                data = my_socket.recv(1024)
+                Recieve = data.decode('utf-8').split(" ")
+                if Recieve[1] ==  "100":
+                    self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
+                    self.wfile.write(b"SIP/2.0 180 Ringing\r\n\r\n")
+                    self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+
+            except TypeError:
+                self.wfile.write(b"SIP/2.0 404 User Not Found\r\n\r\n")
+
+    def ack(self, DATA):
+
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+            user_to_send = DATA[1].split(":")[1]
+            user_to_send_ip = self.dicc_Data.get(user_to_send)[0]
+            user_to_send_port = self.dicc_Data.get(user_to_send)[1]
+            my_socket.connect((user_to_send_ip, int(user_to_send_port)))
+            Response = ("ACK sip:" + user_to_send +
+                        " SIP/2.0\r\n\r\n")
+            my_socket.send(bytes(Response, "utf-8"))
+            print(Response)
+            data = my_socket.recv(1024)
+            print(data.decode("utf-8"))
+
     def handle(self):
         """
         handle method of the server class
@@ -62,15 +104,16 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         for line in self.rfile:
             DATA.append(line.decode('utf-8'))
         DATA = "".join(DATA).split()
-        user = DATA[1].split(":")
-
+        print(DATA)
         if DATA[0] == "REGISTER":
-            if user[1] in self.dicc_Data:
+            user = DATA[1].split(":")[1]
+            port = DATA[1].split(":")[2]
+            if user in self.dicc_Data:
                 tiempo_exp = float(DATA[4]) + time.time()
                 tiempo_exp = time.strftime("%Y-%m-%d %H:%M:%S",
                                            time.gmtime(tiempo_exp))
                 print(" ".join(DATA), "\r\n\r\n")
-                self.dicc_Data[user[1]] = (self.client_address[0],
+                self.dicc_Data[user] = (self.client_address[0],
                                            self.client_address[1],
                                            "Time Register: " + Time_Format,
                                            "Expires: " + tiempo_exp)
@@ -79,8 +122,8 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                 if len(DATA) == 5:
                     if int(DATA[4]) == 0:
                         try:
-                            print("Usuario borrado:", user[1], "\r\n\r\n")
-                            del self.dicc_Data[user[1]]
+                            print("Usuario borrado:", user, "\r\n\r\n")
+                            del self.dicc_Data[user]
                             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                         except KeyError:
                             self.wfile.write(b"SIP/2.0 404 User Not Found\r\n\r\n")
@@ -95,13 +138,16 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                     tiempo_exp = time.strftime("%Y-%m-%d %H:%M:%S",
                                                time.gmtime(tiempo_exp))
                     print(" ".join(DATA), "\r\n\r\n")
-                    self.dicc_Data[user[1]] = (self.client_address[0],
-                                               self.client_address[1],
-                                               "Time Register: " + Time_Format,
-                                               "Expires: " + tiempo_exp)
+                    self.dicc_Data[user] = (self.client_address[0],
+                                            port,
+                                            "Time Register: " + Time_Format,
+                                            "Expires: " + tiempo_exp)
                     self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+        elif DATA[0] == "INVITE":
+            self.invite(DATA)
+        elif DATA[0] == "ACK":
+            self.ack(DATA)
 
-            print(self.dicc_Data)
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001
     # and calls the EchoHandler class to manage the request
